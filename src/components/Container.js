21cca8dc6dfcff9ba9b940d636397f1c6926f90d
rasitdeveloper <frasitkilic@gmail.com>
2022-06-20 16:33:42 +0300

    added Container content

---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import WeatherTable from './WeatherTable';
+import Header from './Header';
+import '../App.css';
+
+function Container() {
+    return (
+      <div>
+          <Header />
+          <WeatherTable />
+      </div>
+    )
+  }
+  
+export default Container
