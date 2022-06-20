ceb474d399ebdf1962662d0f0bd2124aae65db6c
rasitdeveloper <frasitkilic@gmail.com>
2022-06-20 22:12:26 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Container from './components/Container';
 import { WeatherProvider } from './context/WeatherContext'
 
 function App() {
   return (
     <div className='App'>
       <WeatherProvider>
-        
+        <Container />
       </WeatherProvider>
     </div>
   );
